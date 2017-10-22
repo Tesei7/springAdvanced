5ce88baa1f6c343cfab8c6065ccac03b931a3444
Ilya Bochkarev <tesei7@gmail.com>
2017-10-22 21:36:12 +0300

    different return types of controller method

--- src/main/java/advanced/mvc/UserController.java
+++ src/main/java/advanced/mvc/UserController.java
@@ -2,16 +2,31 @@ package advanced.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    @RequestMapping(value = "/greetingModel", method = RequestMethod.GET)
     public String getGreeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping(value = "/greetingMav", method = RequestMethod.GET)
+    public ModelAndView getGreetingModelAndView(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        ModelAndView mav = new ModelAndView("greeting");
+        mav.addObject("name", "dear " + name);
+        return mav;
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    @ModelAttribute("name")
+    public String getGreetingModelAttribute(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return "dearest " + name + "!!";
     }
 }
